a889a26b8c277051989974fe61a90c59823ae192
Egor Ushakov <witluv@icloud.com>
2023-04-30 16:09:37 +0300

    feat: create the main nodes of the document

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 const wrapper = document.createElement('div');
 wrapper.classList.add('wrapper');
 document.body.append(wrapper);
+
+const title = document.createElement('h1');
+title.classList.add('title');
+title.innerText = 'Virtual Keyboard';
+wrapper.append(title);
+
+const textarea = document.createElement('textarea');
+textarea.classList.add('textarea');
+textarea.setAttribute('rows', 10);
+wrapper.append(textarea);
+
+const keyboard = document.createElement('div');
+keyboard.classList.add('keyboard');
+wrapper.append(keyboard);
+
+
+
